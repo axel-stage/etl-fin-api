e6fc275e838798247bdf75ce62c4e1405d1f226a
Axel Stage <dataengineer24@proton.me>
2025-05-25 10:21:50 +0200

    dev: modify main
    - add transform functionality

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
+"""
+main module
+"""
+from config import settings
+
+from extract.api_client import fetch_api_data
+from transform.schema_alpha_vantage import Company, IncomeStatement, BalanceSheet, CashFlow
+
+from utils.logger import logger
+from utils.helper import create_api_endpoint, insert_symbol_value
+
+def etl_company(symbol: str) -> None:
+    # extract
+    url = create_api_endpoint(settings.BASE_URL, "OVERVIEW", symbol, settings.API_KEY)
+    api_data = fetch_api_data(url)
+    # transform
+    company = Company.model_validate(api_data)
+    clean_company = company.model_dump()
+    records = [clean_company]
+    print(records)
+
 def main():
     """Data Pipeline entrypoint"""
 
-    print("Data Pipeline executed")
+    SYMBOL: str = "AMZN"
+
+    etl_company(SYMBOL)
 
 if __name__ == "__main__":
     main()
