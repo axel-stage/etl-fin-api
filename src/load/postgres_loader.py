28cf04aecbcd84e159dd2c364a7798d9665011a4
Axel Stage <dataengineer24@proton.me>
2025-07-01 08:21:31 +0200

    refactoring: modify db loader to switch the relational database

--- src/load/postgres_loader.py
+++ src/load/postgres_loader.py
@@ -1,17 +1,40 @@
 """
 PostgresSQL adapter
 """
+from collections.abc import Callable
 import psycopg as db
 
 from utils.logger import logger
 
 
-def insert_records(connection_info: str, query: str, records: list[dict]) -> None:
+def load_database_engine(database_engine: Callable[[str, str, list[dict]], None]
+) -> Callable[[str, str, list[dict]], None]:
     """
-    Connects to a database and inserts a list of records via a query
+    Higher-order function to load data into a database using the provided database engine function.
 
     Args:
-        connection_info (str): The database connection.
+        database_engine: A function that performs the actual DB insert.
+        connection: A database connection string.
+        query: SQL insert query to use.
+
+    Returns:
+        A function that accepts data and executes the insert using the database engine.
+    """
+    def loader(connection: str, query: str, data: list[dict]) -> None:
+        try:
+            logger.info("Attempting to load data to the database...")
+            database_engine(connection, query, data)
+            logger.info("Data successfully loaded.")
+        except Exception as error:
+            logger.error(f"Failed to load data: {error}")
+    return loader
+
+def insert_into_postgresql(connection_info: str, query: str, records: list[dict]) -> None:
+    """
+    Connects to a PostgreSQL database and inserts a list of records via a query
+
+    Args:
+        connection_info (str): The database connection string.
         query (str): The SQL query.
         records (list[dict]): The records to insert.
 
@@ -24,5 +47,19 @@ def insert_records(connection_info: str, query: str, records: list[dict]) -> Non
             with connection.cursor() as running_cursor:
                 try:
                     running_cursor.execute(query, record)
-                except db.Error as err:
-                    logger.error(err)
+                except db.Error as error:
+                    logger.error(error)
+
+def insert_into_mysql(connection_info: str, query: str, records: list[dict]) -> None:
+    """
+    Connects to a MySQL database and inserts a list of records via a query
+
+    Args:
+        connection_info (str): The database connection string.
+        query (str): The SQL query.
+        records (list[dict]): The records to insert.
+
+    Returns:
+        None
+    """
+    pass
